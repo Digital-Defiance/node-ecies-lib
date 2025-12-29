65d197e9dbe1a3fe0325fb1c940ea3fbd209065f
Jessica Mulein <jessica@digitaldefiance.org>
2025-12-28 23:02:55 -0800

    v4.7.1

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export * from './lib';
 export * from './constants';
 export * from './enumerations';
 export * from './i18n';
-export type { IMember } from './interfaces';
+export * from './interfaces';
 export * from './member';
 export * from './types';
 
